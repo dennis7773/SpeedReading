e2a7147cbf0b5acd517e13030863651390cf57c2
dennis7773 <dennis.heindl@t-online.de>
2015-12-04 22:16:23 +0100

    Add NavigationDrawer

--- app/src/main/java/com/dheindl/speedreading/MainActivity.java
+++ app/src/main/java/com/dheindl/speedreading/MainActivity.java
@@ -24,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
+                startActivity(new Intent(this, Main2Activity.class));
             }
         });
     }
