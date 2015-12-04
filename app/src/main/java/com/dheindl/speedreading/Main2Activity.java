2a45f4172a06c3ee5a40b9f1b69e665e3ae46f65
dennis7773 <dennis.heindl@t-online.de>
2015-12-04 23:40:28 +0100

    Test

--- app/src/main/java/com/dheindl/speedreading/Main2Activity.java
+++ app/src/main/java/com/dheindl/speedreading/Main2Activity.java
@@ -28,7 +28,8 @@ public class Main2Activity extends AppCompatActivity
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                        .setAction("Action", null)
+                        .show();
             }
         });
 
